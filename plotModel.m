8f90f2f2cc56dbb281f1ad06cbdb913cfdbd94bb
ezra <ezra@soniccloud.com>
2020-06-21 14:38:23 -0700

    fix stuff

--- plotModel.m
+++ plotModel.m
@@ -1,12 +1,21 @@
 function plotModel(axes, selection, results)
 %TODO: handle arrays of selections with multple plots
     title(axes, selection)
+    disp("plotModel()");
+    disp(selection);
     switch(selection)
         case 'Output1'
+            disp("plotting Output1")
             cla(axes);
-            plot( axes, results.x, results.y);
+            % ... do whatever with the Results class to get something to
+            % plot
+            xlabel(axes, "z * x");
+            plot( axes, results.x, results.z .* results.x);
+            
         case 'Output2'
+            disp("plotting Output2")
             cla(axes);
-            plot( axes, results.x, results.z);
+            xlabel(axes, "z + y^2");
+            plot( axes, results.x, results.z + (results.y .* results.y));
     end
 end
